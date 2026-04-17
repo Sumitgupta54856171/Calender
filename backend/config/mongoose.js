671586d10b4300aee7c39aa83b6f798b0e5ada27
Sumitgupta54856171 <Sg54856171@gmail.com>
2026-04-17 01:33:53 -0700

    Update MongoDB connection string handling

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -3,7 +3,7 @@ const  mongoose = require("mongoose")
 
 const mongoconnect= async()=>{
     mongoose
-    .connect(process.env.mongodb_url||"mongodb://51.81.22.42:27017")
+    .connect(process.env.mongodb_url)
     .then(()=>console.log("mongodb connected"))
     .catch((error)=>{console.log(`mongodb server created the error this is error ${error}`)})
 }
