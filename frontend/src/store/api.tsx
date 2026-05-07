d1815977e3ddbc22714e0a0c032f2af07b948b32
Sumitgupta54856171 <sg54856171.com>
2026-05-07 03:12:09 +0000

    change in the backend urk

--- frontend/src/store/api.tsx
+++ frontend/src/store/api.tsx
@@ -4,7 +4,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath:'api',
     baseQuery:fetchBaseQuery({
-        baseUrl:'https://51-81-22-42.nip.io/api',
+        baseUrl:'https://139.59.58.145.nip.io/api',
     }),
     endpoints:(builder)=>({
         getSessions:builder.query({
